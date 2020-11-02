2c012e2d4a867c28630c81685854cf4835cc2ef1
pnguyen215 <nguyenhuuphuocit97@gmail.com>
2020-11-02 13:03:27 +0700

    fixed: pick up all together into service #11

--- src/main/java/com/phuocnguyen/app/sivaosactions/Configurer/AuthenticationRemoteTokenServiceSIVAOS/ResourceServerConfigure.java
+++ src/main/java/com/phuocnguyen/app/sivaosactions/Configurer/AuthenticationRemoteTokenServiceSIVAOS/ResourceServerConfigure.java
@@ -1,13 +1,9 @@
 package com.phuocnguyen.app.sivaosactions.Configurer.AuthenticationRemoteTokenServiceSIVAOS;
 
 import com.sivaos.Configurer.AuthenticationRemoteTokenServiceSIVAOS.CustomRemoteTokenServiceConfigure;
-import com.sivaos.Configurer.AuthenticationRemoteTokenServiceSIVAOS.CustomTokenExtractor;
-import com.sivaos.Service.SIVAOSServiceImplement.SIVAOSAccessDeniedHandlerImplement;
-import com.sivaos.Service.SIVAOSServiceImplement.SIVAOSAuthenticationEntryPointImplement;
-import com.sivaos.Utils.PropertiesUtils;
+import com.sivaos.Service.SIVAOSServiceImplement.SIVAOSAuthenticationServiceImplement;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
@@ -20,16 +16,13 @@ import org.springframework.security.oauth2.provider.token.ResourceServerTokenSer
 @EnableResourceServer
 public class ResourceServerConfigure extends ResourceServerConfigurerAdapter {
 
-    protected static void loadProperties() {
-        PropertiesUtils.setApplicationSource("application-config-keys.yml");
-        PropertiesUtils.loadProperties();
-    }
+    SIVAOSAuthenticationServiceImplement sivaOsAuthenticationService;
+
 
     @Override
     public void configure(ResourceServerSecurityConfigurer resources) {
-        loadProperties();
-        resources.resourceId(PropertiesUtils.readPropertiesByAttributed("resourcesIds")).authenticationManager(authenticationManagerBean())
-                .tokenExtractor(new CustomTokenExtractor()).tokenServices(tokenService()).authenticationEntryPoint(new SIVAOSAuthenticationEntryPointImplement()).accessDeniedHandler(new SIVAOSAccessDeniedHandlerImplement());
+        sivaOsAuthenticationService = new SIVAOSAuthenticationServiceImplement();
+        sivaOsAuthenticationService.configureResourceServerSecurityConfigurer(resources, tokenService(), authenticationManagerBean());
     }
 
     @Bean
@@ -48,26 +41,7 @@ public class ResourceServerConfigure extends ResourceServerConfigurerAdapter {
     @Override
     public void configure(HttpSecurity http) throws Exception {
 
-        http
-                .authorizeRequests()
-                .antMatchers("/swagger-ui.html").permitAll()
-                .antMatchers("/webjars/**").permitAll()
-                .antMatchers("/**/actuator/**").permitAll()
-                .antMatchers("/swagger-resources/**").permitAll()
-                .antMatchers("/v2/api-docs/**").permitAll()
-                .antMatchers("/api/v18/actions/download-ldp").permitAll()
-                .antMatchers(HttpMethod.GET, "/**").access("#oauth2.hasScope('trust')")
-                .antMatchers(HttpMethod.POST, "/**").access("#oauth2.hasScope('write')")
-                .antMatchers(HttpMethod.PATCH, "/**").access("#oauth2.hasScope('write')")
-                .antMatchers(HttpMethod.PUT, "/**").access("#oauth2.hasScope('write')")
-                .antMatchers(HttpMethod.DELETE, "/**").access("#oauth2.hasScope('write')")
-                .and()
-                .headers().addHeaderWriter((request, response) -> {
-            response.addHeader("Access-Control-Allow-Origin", "*");
-            if (request.getMethod().equals("OPTIONS")) {
-                response.setHeader("Access-Control-Allow-Methods", request.getHeader("Access-Control-Request-Method"));
-                response.setHeader("Access-Control-Allow-Headers", request.getHeader("Access-Control-Request-Headers"));
-            }
-        });
+        sivaOsAuthenticationService = new SIVAOSAuthenticationServiceImplement();
+        sivaOsAuthenticationService.configureHttpSecurity(http);
     }
 }
