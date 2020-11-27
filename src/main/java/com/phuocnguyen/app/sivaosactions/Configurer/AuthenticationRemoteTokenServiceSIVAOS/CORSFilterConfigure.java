e7c26d548970fe538437516621d0b434afca262f
pnguyen215 <nguyenhuuphuocit97@gmail.com>
2020-11-27 22:42:50 +0700

    updated: config csrf filter chain #14

--- src/main/java/com/phuocnguyen/app/sivaosactions/Configurer/AuthenticationRemoteTokenServiceSIVAOS/CORSFilterConfigure.java
+++ src/main/java/com/phuocnguyen/app/sivaosactions/Configurer/AuthenticationRemoteTokenServiceSIVAOS/CORSFilterConfigure.java
@@ -4,9 +4,6 @@ import com.sivaos.Configurer.CustomFilterRequest.TypeSafeRequest;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.*;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 @Component
 public class CORSFilterConfigure implements Filter {
@@ -22,5 +19,6 @@ public class CORSFilterConfigure implements Filter {
     }
 
     public void init(FilterConfig fConfig) {
+        TypeSafeRequest.init(fConfig);
     }
 }
--- src/main/java/com/phuocnguyen/app/sivaosactions/Controller/UsersActiveController.java
+++ src/main/java/com/phuocnguyen/app/sivaosactions/Controller/UsersActiveController.java
@@ -6,9 +6,9 @@ import com.sivaos.Model.Response.SIVAResponseDTO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -27,16 +27,18 @@ public class UsersActiveController extends BaseSIVAOSController {
 
     private final SIVAJDBCConnectConfigurer sivajdbcConnectConfigurer;
 
+    @Value("${spring.profiles.active}")
+    private String profileActive;
+
     @Autowired
     public UsersActiveController(SIVAJDBCConnectConfigurer sivajdbcConnectConfigurer) {
         this.sivajdbcConnectConfigurer = sivajdbcConnectConfigurer;
     }
 
     @GetMapping(ENDPOINT_USER_CURRENT)
-    @Async("threadPoolExecutor")
     public @ResponseBody
     ResponseEntity<?> snagUsers() {
-        Connection connection = sivajdbcConnectConfigurer.getConnection();
+        Connection connection = sivajdbcConnectConfigurer.getConnection(profileActive);
         logger.info("current username: {}", currentUsername);
         snagUserAsQuery(currentUsername, connection);
         snagRoleIdsAsQuery(currentUsername, connection);
